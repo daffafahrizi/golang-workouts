382e50504a4025b2251abaac6bd418c40480f8f7
Daffa Fahrizi <daffa.anis@ui.ac.id>
2024-10-09 17:37:26 +0700

    Checkpoint 5

---
+++ Pemrograman Go Dasar/C9 CLI Flag Parser (kingpinv2)/main.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/alecthomas/kingpin/v2"
+)
+
+var (
+	argAppName = kingpin.Arg("name", "Application name").Required().String()
+	argPort = kingpin.Arg("port", "web server port").Default("9000").Int()
+)
+
+func main(){
+	kingpin.Parse()
+
+	appName := *argAppName
+	port := fmt.Sprintf(":%d", *argPort)
+
+	fmt.Printf("Starting %s on port %s\n", appName, port)
+
+	e := echo.New()
+	e.GET("/index", func(c echo.Context) (err error){
+		return c.JSON(http.StatusOK, true)
+
+	})
+	e.Logger.Fatal(e.Start(port))
+}
